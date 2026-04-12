6670a67cef693db1d3a81a99a3166f1ed3721796
Jordy CABANNES <jordycabannes@msn.com>
2026-04-12 16:50:13 +0200

    chore(docker): rename lemonldap database for dev environment

--- docker/dev/resources/init_lemonldap_db.sql
+++ docker/dev/resources/init_lemonldap_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS accounts (
+CREATE TABLE IF NOT EXISTS lemonldap_accounts (
     username VARCHAR(255) NOT NULL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     cn       VARCHAR(255),
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS accounts (
     roles    VARCHAR(255)
 );
 
-INSERT INTO accounts (username, password, cn, mail, roles) VALUES ('admin', 'password', 'admin name', 'admin@example.com', 'admin,user');
-INSERT INTO accounts (username, password, cn, mail, roles) VALUES ('user1', 'password', 'user1 name', 'user1@example.com', 'user');
-INSERT INTO accounts (username, password, cn, mail, roles) VALUES ('user2', 'password', 'user2 name', 'user2@example.com', 'user');
+INSERT INTO lemonldap_accounts (username, password, cn, mail, roles) VALUES ('admin', 'password', 'admin name', 'admin@example.com', 'admin,user');
+INSERT INTO lemonldap_accounts (username, password, cn, mail, roles) VALUES ('user1', 'password', 'user1 name', 'user1@example.com', 'user');
+INSERT INTO lemonldap_accounts (username, password, cn, mail, roles) VALUES ('user2', 'password', 'user2 name', 'user2@example.com', 'user');

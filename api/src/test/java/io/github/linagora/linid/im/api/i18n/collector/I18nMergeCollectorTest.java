05c435d7e3221c1c2df23f9846daccf0a75388ca
cchevalier <cchevalier@linagora.com>
2026-04-16 10:48:30 +0200

    style: fix trailing whitespace in license headers

--- api/src/test/java/io/github/linagora/linid/im/api/i18n/collector/I18nMergeCollectorTest.java
+++ api/src/test/java/io/github/linagora/linid/im/api/i18n/collector/I18nMergeCollectorTest.java
@@ -6,8 +6,8 @@
  * any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
  * LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
  * which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
- * of the interface window, the display of the "You are using the Open Source and free version of LinID™, powered by
- * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!" infobox and in the e-mails
+ * of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+ * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
  * sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
  * as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
  * and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA

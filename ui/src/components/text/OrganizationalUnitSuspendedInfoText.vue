25233054cd6ec7d5d6000f3f81e0cd5354561752
cchevalier <cchevalier@linagora.com>
2026-05-28 16:50:44 +0200

    feat(ui): add OU lifecycle composable and presentation components

---
+++ ui/src/components/text/OrganizationalUnitSuspendedInfoText.vue
@@ -0,0 +1,71 @@
+<!--
+  Copyright (C) 2026 Linagora
+
+  This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+  Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+  any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
+  LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
+  which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
+  of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+  Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
+  sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
+  as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
+  and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA
+  intellectual property rights over its trademarks and commercial brands. Other Additional Terms apply, see
+  <http://www.linagora.com/licenses/> for more details.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+  details.
+
+  You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+  LinID Identity Manager along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero
+  General Public License version 3 and <http://www.linagora.com/licenses/> for the Additional Terms applicable to the
+  LinID Identity Manager software.
+-->
+
+<template>
+  <div
+    class="text-warning row q-gutter-x-sm organizational-unit-suspended-info-text"
+    data-cy="organizational-unit-suspended-info-text"
+  >
+    <q-icon
+      v-bind="uiProps.icon"
+      class="organizational-unit-suspended-info-text--icon"
+    />
+    <span
+      class="organizational-unit-suspended-info-text--message"
+      data-cy="organizational-unit-suspended-info-text_message"
+    >
+      {{ t('message', { date: startDate }) }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  type LinidQIconProps,
+  useScopedI18n,
+  useUiDesign,
+} from '@linagora/linid-im-front-corelib';
+import { useCommonMapper } from 'src/composables/useCommonMapper';
+import type { OrganizationalUnitInfoTextProps } from 'src/types/organizationalUnitInfoTextProps';
+import { computed } from 'vue';
+
+const props = defineProps<OrganizationalUnitInfoTextProps>();
+
+const localUiNamespace = `organizational-unit-suspended-info-text`;
+const { toDate } = useCommonMapper();
+const { t } = useScopedI18n('OrganizationalUnitSuspendedInfoText');
+const { ui } = useUiDesign();
+
+const startDate = computed(() =>
+  toDate(props.organizationalUnitStatus.suspensionPeriod?.start)
+);
+
+const uiProps = {
+  icon: ui<LinidQIconProps>(localUiNamespace, 'q-icon'),
+};
+</script>
+
+<style scoped></style>
---
+++ ui/src/types/organizationalUnitLifecycleUi.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2026 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
+ * LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
+ * which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+ * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
+ * sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
+ * as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
+ * and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA
+ * intellectual property rights over its trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+ * LinID Identity Manager along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero
+ * General Public License version 3 and <http://www.linagora.com/licenses/> for the Additional Terms applicable to the
+ * LinID Identity Manager software.
+ */
+
+import type { MenuItem } from '@linagora/linid-im-front-corelib';
+
+/**
+ * Whitelist of dotted lifecycle action keys exposed in the OU dropdowns and
+ * banner buttons.
+ *
+ * The constant is the source of truth for the union
+ * {@link OrganizationalUnitLifecycleAction} and for the runtime guard applied
+ * when the federated DropdownButton emits a click.
+ */
+export const ORGANIZATIONAL_UNIT_LIFECYCLE_ACTIONS = [
+  'suspension.immediate',
+  'suspension.scheduled',
+  'suspension.modify-end',
+  'reactivation.immediate',
+] as const;
+
+/**
+ * Union of all valid OU lifecycle action keys. Derived from
+ * {@link ORGANIZATIONAL_UNIT_LIFECYCLE_ACTIONS} so the runtime whitelist and
+ * the compile-time type stay in sync automatically.
+ */
+export type OrganizationalUnitLifecycleAction =
+  (typeof ORGANIZATIONAL_UNIT_LIFECYCLE_ACTIONS)[number];
+
+/**
+ * UI projection of the OU lifecycle state, derived deterministically from an
+ * {@link import('src/types/organizationalUnits').OrganizationalUnitStatus}.
+ * All template logic must consume this object only, to avoid recomputing the
+ * same rules in multiple places.
+ */
+export interface OrganizationalUnitLifecycleUi {
+  /**
+   * True when the status badge must be rendered next to the page title.
+   */
+  showBadge?: boolean;
+  /**
+   * True when the yellow suspension banner must be rendered (OU currently
+   * suspended).
+   */
+  showSuspendedBanner?: boolean;
+  /**
+   * True when the yellow "will be suspended on …" info text must be rendered
+   * (suspension scheduled in the future).
+   */
+  showWillSuspendInfoText?: boolean;
+  /**
+   * Menu items for the suspension dropdown. Undefined when no suspension
+   * action applies to the current state, for example when already suspended.
+   */
+  suspensionMenuItems?: MenuItem[];
+  /**
+   * Menu items for the activation/reactivation dropdown. Undefined when no
+   * activation action applies, for example when the OU is not suspended.
+   */
+  activationMenuItems?: MenuItem[];
+}

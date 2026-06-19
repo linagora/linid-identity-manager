df4e719c012d71c6c7793943f577abded2408746
Julia Jamin <julia.jamin15@gmail.com>
2026-06-19 10:48:07 +0200

    chore(api): create entity and repository of user preferences

---
+++ api/src/main/java/io/github/linagora/linid/im/api/persistence/repository/UserPreferenceRepository.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020-2026 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
+ * LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
+ * which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+ * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
+ * sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
+ * as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
+ * and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA
+ * intellectual property rights over its trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+ * LinID Identity Manager along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero
+ * General Public License version 3 and <http://www.linagora.com/licenses/> for the Additional Terms applicable to the
+ * LinID Identity Manager software.
+ */
+
+package io.github.linagora.linid.im.api.persistence.repository;
+
+import io.github.linagora.linid.im.api.persistence.model.UserPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Spring Data JPA repository for {@link UserPreference}.
+ *
+ * <p>Extends {@link JpaSpecificationExecutor} to support dynamic filtering
+ * via {@code spring-query-filter} specifications.</p>
+ */
+public interface UserPreferenceRepository extends JpaRepository<UserPreference, UUID>,
+        JpaSpecificationExecutor<UserPreference> {
+
+    /**
+     * Retrieves all preferences associated with the specified user.
+     *
+     * @param userId the unique identifier of the user
+     * @return a list of {@link UserPreference} entries for the user,
+     *     or an empty list if no preferences exist
+     */
+    List<UserPreference> findByUserId(UUID userId);
+
+    /**
+     * Retrieves a user preference matching the specified user and preference key.
+     *
+     * <p>Relies on the unique ({@code userId}, {@code key}) constraint, ensuring
+     * that at most one matching preference can exist.</p>
+     *
+     * @param userId the unique identifier of the user
+     * @param key the preference key
+     * @return an {@link Optional} containing the matching {@link UserPreference}
+     *     if found, or {@link Optional#empty()} otherwise
+     */
+    Optional<UserPreference> findByUserIdAndKey(UUID userId, String key);
+
+}

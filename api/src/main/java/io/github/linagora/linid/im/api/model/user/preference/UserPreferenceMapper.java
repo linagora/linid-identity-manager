82a3ef25f0ba89571b196f90e2d7cba74bf11740
Julia Jamin <julia.jamin15@gmail.com>
2026-06-23 18:02:09 +0200

    feat(api): add preference DTO, record and mapper

---
+++ api/src/main/java/io/github/linagora/linid/im/api/model/user/preference/UserPreferenceMapper.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2020-2026 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
+ * LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
+ * which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+ * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
+ * sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
+ * as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
+ * and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA
+ * intellectual property rights over its trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+ * LinID Identity Manager along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero
+ * General Public License version 3 and <http://www.linagora.com/licenses/> for the Additional Terms applicable to the
+ * LinID Identity Manager software.
+ */
+
+package io.github.linagora.linid.im.api.model.user.preference;
+
+import io.github.linagora.linid.im.api.model.user.UserPrincipal;
+import io.github.linagora.linid.im.api.persistence.model.UserPreference;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/**
+ * MapStruct mapper for the user preference API contract.
+ *
+ * <p>Handles three conversions:</p>
+ * <ul>
+ *   <li>{@link UserPreferenceRecord} + {@link UserPrincipal} → {@link UserPreference} entity
+ *       (creation/update input);</li>
+ *   <li>{@link UserPreference} entity → {@link UserPreferenceDTO} (single-preference response);</li>
+ *   <li>a list of {@link UserPreference} entities → a {@code {key: value}} map, implemented as a
+ *       hand-written {@code default} method since it is an aggregation MapStruct cannot generate.</li>
+ * </ul>
+ */
+@Mapper(componentModel = "spring")
+public interface UserPreferenceMapper {
+
+    /**
+     * Creates a new {@link UserPreference} entity from a {@link UserPreferenceRecord} and the calling principal.
+     *
+     * <p>The {@code email} is resolved from the authenticated principal (never from the request body),
+     * and {@code createdBy} / {@code updatedBy} are set to the principal's identifier. The {@code id} and
+     * audit timestamps ({@code insertDate}, {@code updateDate}) are left unset and populated by the database.</p>
+     *
+     * @param record        the creation or update request record
+     * @param userPrincipal the authenticated principal performing the operation
+     * @return a partially populated {@link UserPreference} entity
+     */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "insertDate", ignore = true)
+    @Mapping(target = "updateDate", ignore = true)
+    @Mapping(target = "createdBy", source = "userPrincipal.id")
+    @Mapping(target = "updatedBy", source = "userPrincipal.id")
+    @Mapping(target = "email", source = "userPrincipal.email")
+    @Mapping(target = "key", source = "record.key")
+    @Mapping(target = "value", source = "record.value")
+    UserPreference toUserPreference(UserPreferenceRecord record, UserPrincipal userPrincipal);
+
+    /**
+     * Converts a {@link UserPreference} entity to a {@link UserPreferenceDTO}.
+     *
+     * @param userPreference the user preference entity
+     * @return the corresponding DTO
+     */
+    UserPreferenceDTO toDTO(UserPreference userPreference);
+}

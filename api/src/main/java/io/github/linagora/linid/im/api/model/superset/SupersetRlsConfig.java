abb6799c119f9aee9e3dbdd799f1c735ba50c92b
Zorin95670 <moittie.vincent@gmail.com>
2026-08-13 16:36:57 +0200

    chore(api): add models to manage superset

---
+++ api/src/main/java/io/github/linagora/linid/im/api/model/superset/SupersetRlsConfig.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2020-2026 Linagora
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version, provided you comply with the Additional Terms applicable for LinID Identity Manager software by
+ * LINAGORA pursuant to Section 7 of the GNU Affero General Public License, subsections (b), (c), and (e), pursuant to
+ * which these Appropriate Legal Notices must notably (i) retain the display of the "LinID™" trademark/logo at the top
+ * of the interface window, the display of the “You are using the Open Source and free version of LinID™, powered by
+ * Linagora © 2009–2013. Contribute to LinID R&D by subscribing to an Enterprise offer!” infobox and in the e-mails
+ * sent with the Program, notice appended to any type of outbound messages (e.g. e-mail and meeting requests) as well
+ * as in the LinID Identity Manager user interface, (ii) retain all hypertext links between LinID Identity Manager
+ * and https://linid.org/, as well as between LINAGORA and LINAGORA.com, and (iii) refrain from infringing LINAGORA
+ * intellectual property rights over its trademarks and commercial brands. Other Additional Terms apply, see
+ * <http://www.linagora.com/licenses/> for more details.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License and its applicable Additional Terms for
+ * LinID Identity Manager along with this program. If not, see <http://www.gnu.org/licenses/> for the GNU Affero
+ * General Public License version 3 and <http://www.linagora.com/licenses/> for the Additional Terms applicable to the
+ * LinID Identity Manager software.
+ */
+
+package io.github.linagora.linid.im.api.model.superset;
+
+
+/**
+ * Configuration used to apply Row Level Security (RLS) rules
+ * to Superset dashboard requests.
+ *
+ * @param slug       the Superset dashboard slug associated with this RLS configuration
+ * @param rlsEnabled whether RLS must be applied for the dashboard
+ * @param datasetId the Superset dataset identifier on which the RLS rule is applied
+ * @param entity     the entity name used to resolve the value required by the RLS rule
+ * @param attribute  the entity attribute used to build the RLS condition
+ */
+public record SupersetRlsConfig(
+    String slug,
+    boolean rlsEnabled,
+    int datasetId,
+    String entity,
+    String attribute
+) {
+}

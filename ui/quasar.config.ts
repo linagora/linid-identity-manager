03517cfd6ba1706925aefb65af30777fa0d7c1f6
Adrien <acere@linagora.com>
2026-09-21 17:31:26 +0200

    build: use the Inter font from its npm package

--- ui/quasar.config.ts
+++ ui/quasar.config.ts
@@ -37,9 +37,7 @@ export default defineConfig((ctx) => {
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
-      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
       'material-icons', // optional, you are not bound to it
     ],
 
